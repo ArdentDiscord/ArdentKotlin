6d8d1d339b6d135fbd5f7113cb28fc6cc20fa762
adamint <adam@adamratzman.com>
2017-08-07 21:26:04 -0400

    how much more can i procrastinate finishing this?

--- src/main/kotlin/commands/games/Games.kt
+++ src/main/kotlin/commands/games/Games.kt
@@ -1,10 +1,10 @@
 package commands.games
 
+import com.sun.org.apache.xpath.internal.operations.Bool
 import events.Category
 import events.Command
 import main.factory
 import main.waiter
-import net.dv8tion.jda.core.Permission
 import net.dv8tion.jda.core.entities.Guild
 import net.dv8tion.jda.core.entities.Member
 import net.dv8tion.jda.core.entities.TextChannel
@@ -21,28 +21,70 @@ import java.util.concurrent.TimeUnit
 val invites = ConcurrentHashMap<String, Game>()
 
 class BlackjackGame(channel: TextChannel, creator: String, playerCount: Int, isPublic: Boolean) : Game(GameType.BLACKJACK, channel, creator, playerCount, isPublic) {
+    val roundResults = mutableListOf<Round>()
     override fun onStart() {
         val ardent = channel.guild.selfMember
-        val users = players.map { it.toUser()!! }.toList()
+        val user = players.map { it.toUser()!! }.toList()[0]
+
 
     }
-    data class Round(val dealerValue: Int, val userData: List<Pair<String, Int>>) {
-        fun winners(): List<Pair<String, Int>> {
-            val winners = mutableListOf<Pair<String, Int>>()
-            userData.forEach { if (it.second in (dealerValue + 1)..21) winners.add(it) }
-            return winners
-        }
+    fun doRound(user: User) {
 
-        fun losers(): List<Pair<String, Int>> {
-            val losers = mutableListOf<Pair<String, Int>>()
-            userData.forEach { if (it.second in 1..(dealerValue - 1)) losers.add(it) }
-            return losers
-        }
+    }
 
-        fun tied(): List<String> {
-            val tied = mutableListOf<String>()
-            userData.forEach { if (it.second == dealerValue) tied.add(it.first) }
-            return tied
+    class Round(val won: Boolean, val userHand: Hand, val dealerHand: Hand)
+    class Hand(val cards: MutableList<Card>) {
+        val random = Random()
+        fun with(cardAmount: Int) : Hand {
+            cards.add(generate())
+            cards.add(generate())
+            return this
+        }
+        fun generate() : Card {
+            val card = Card(Suit.values()[random.nextInt(4)], BlackjackValue.values()[random.nextInt(13)])
+            if (cards.contains(card)) return generate()
+            else return card
+        }
+    }
+    data class Card(val suit: Suit, val value: BlackjackValue) {
+        override fun toString(): String {
+            return "$suit$value"
+        }
+    }
+    enum class Suit {
+        HEART, SPADE, CLUB, DIAMOND;
+        override fun toString(): String {
+            return when (this) {
+                HEART -> "❤"
+                SPADE -> "♠"
+                CLUB -> "♧"
+                DIAMOND -> "♦"
+            }
+        }
+    }
+    enum class BlackjackValue(var representation: Int) {
+        TWO(2),
+        THREE(3),
+        FOUR(4),
+        FIVE(5),
+        SIX(6),
+        SEVEN(7),
+        EIGHT(8),
+        NINE(9),
+        TEN(10),
+        JACK(10),
+        QUEEN(10),
+        KING(10),
+        ACE(11);
+
+        override fun toString(): String {
+            return when(this) {
+                ACE -> "A"
+                KING -> "K"
+                QUEEN -> "Q"
+                JACK -> "J"
+                else -> representation.toString()
+            }
         }
     }
 }
@@ -60,39 +102,41 @@ class CoinflipGame(channel: TextChannel, creator: String, playerCount: Int, isPu
             val roundResults = Round()
             val rand = SecureRandom()
             channel.send(channel.guild.selfMember, "${users.map { it.asMention }.concat()}, you're up in round **$round** of **5**. Type `heads` now to guess **Heads** or `tails` to guess **Tails** - you have __15__ seconds!")
-                factory.executor.execute {
-                    val answered = mutableListOf<String>()
-                    waiter.gameChannelWait(channel.id, { message ->
-                        val it = message.author
-                        if (players.contains(it.id) && !answered.contains(it.id)) {
-                            val content = message.content
-                            val isHeads: Boolean
-                            when {
-                                content.startsWith("he", true) -> isHeads = true
-                                content.startsWith("ta", true) -> isHeads = false
-                                else -> {
-                                    channel.send(it, "${it.asMention} didn't type **heads** or **tails** - Heads will be assumed")
-                                    isHeads = true
-                                }
+            factory.executor.execute {
+                val answered = mutableListOf<String>()
+                waiter.gameChannelWait(channel.id, { message ->
+                    val it = message.author
+                    if (players.contains(it.id) && !answered.contains(it.id)) {
+                        val content = message.content
+                        val isHeads: Boolean
+                        when {
+                            content.startsWith("he", true) -> isHeads = true
+                            content.startsWith("ta", true) -> isHeads = false
+                            else -> {
+                                channel.send(it, "${it.asMention} didn't type **heads** or **tails** - Heads will be assumed")
+                                isHeads = true
                             }
-                            if (rand.nextBoolean() == isHeads) {
-                                roundResults.winners.add(it.id)
-                                channel.send(it, "Congrats, ${it.asMention} guessed correctly!")
-                            } else {
-                                channel.send(it, "Sadly, ${it.asMention} didn't guess correctly ;(")
-                                roundResults.losers.add(it.id)
-                            }
-                            answered.add(it.id)
                         }
-                    })
-                }
+                        if (rand.nextBoolean() == isHeads) {
+                            roundResults.winners.add(it.id)
+                            channel.send(it, "Congrats, ${it.asMention} guessed correctly!")
+                        } else {
+                            channel.send(it, "Sadly, ${it.asMention} didn't guess correctly ;(")
+                            roundResults.losers.add(it.id)
+                        }
+                        answered.add(it.id)
+                    }
+                })
+            }
 
             Thread.sleep(15000)
             val missedRound = mutableListOf<String>()
-            users.forEach { if (!roundResults.losers.contains(it.id) && !roundResults.winners.contains(it.id)) {
-                roundResults.losers.add(it.id)
-                missedRound.add(it.id)
-            } }
+            users.forEach {
+                if (!roundResults.losers.contains(it.id) && !roundResults.winners.contains(it.id)) {
+                    roundResults.losers.add(it.id)
+                    missedRound.add(it.id)
+                }
+            }
             if (missedRound.size > 0) channel.send(channel.guild.selfMember, "The following user(s) missed this round and therefore lost: ${missedRound.toUsers()}")
             roundResults
 
@@ -180,16 +224,15 @@ class Games : Command(Category.GAMES, "minigames", "who's the most skilled? play
                     channel.selectFromList(member, "Which type of game would you like to create?", GameType.values().map { it.readable }.toMutableList(), {
                         selected ->
                         val gameType = GameType.values()[selected]
-                        if (guild.hasGameType(gameType) && !member.hasDonationLevel(channel, DonationLevel.INTERMEDIATE, failQuietly = true)){
+                        if (guild.hasGameType(gameType) && !member.hasDonationLevel(channel, DonationLevel.INTERMEDIATE, failQuietly = true)) {
                             channel.send(member, "There can only be one of this type of game active at a time in a server!. **Pledge $5 a month or buy the Intermediate rank at " +
                                     "https://ardentbot.com/support_us to start more than one game per type at a time**")
-                        }
-                        else {
-                            channel.selectFromList(member, "Would you like this game of ${gameType.readable} to be open to everyone to join?", mutableListOf("Yes", "No"), {
-                                public ->
-                                val isPublic = public == 0
-                                when (gameType) {
-                                    GameType.COINFLIP -> {
+                        } else {
+                            when (gameType) {
+                                GameType.COINFLIP -> {
+                                    channel.selectFromList(member, "Would you like this game of ${gameType.readable} to be open to everyone to join?", mutableListOf("Yes", "No"), {
+                                        public ->
+                                        val isPublic = public == 0
                                         channel.send(member, "How many players would you like in this game? Type `none` to set the limit as 999 (effectively no limit)")
                                         waiter.waitForMessage(Settings(member.user.id, channel.id, guild.id), {
                                             playerCount ->
@@ -198,22 +241,15 @@ class Games : Command(Category.GAMES, "minigames", "who's the most skilled? play
                                             gamesInLobby.add(game)
 
                                         })
-                                    }
-                                    GameType.BLACKJACK -> {
-                                        channel.send(member, "How many players would you like in this game? You can also type `1` to play solo")
-                                        waiter.waitForMessage(Settings(member.id(), channel.id, guild.id), {
-                                            playerCount ->
-                                            val count = playerCount.content.toIntOrNull() ?: 1
-                                            val game = BlackjackGame(channel, member.id(), count, isPublic)
-                                            if (count > 1) {
-                                                gamesInLobby.add(game)
-                                            }
-                                            else game.startEvent()
-                                        })
-                                    }
+                                    })
                                 }
-                                // TODO("Fill in the other games")
-                            })
+                                GameType.BLACKJACK -> {
+                                    channel.send(member, "This is a solo game; it'll start in a second!")
+                                    BlackjackGame(channel, member.id(), 1, false).startEvent()
+                                }
+                            }
+                            // TODO("Fill in the other games")
+
                         }
                     })
                 }
