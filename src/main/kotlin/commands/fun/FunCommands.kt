e12f34083419aad842c6e816a4c9d016c714ea43
adamint <adam@adamratzman.com>
2017-09-16 22:43:26 -0400

    1.1.11 -> commit 2/5 - fun commands done

--- src/main/kotlin/commands/fun/FunCommands.kt
+++ src/main/kotlin/commands/fun/FunCommands.kt
@@ -23,47 +23,47 @@ class Roll : Command(Category.FUN, "roll", "use our customizable system to roll 
         val help = """You can use this command with the following format: **sides**x**amount**, where `amount` represents the amount of times
 you want to roll the die, and `sides` represents the amount of sides that this die will have.
 
-**Example**: *${event.guild.getPrefix()}roll 9x4* - this would roll a 9-sided die 4 times
+**Example**: *{0}roll 9x4* - this would roll a 9-sided die 4 times
 
-__Please Note__: You are limited to 999999x10, meaning that at maximum you can roll a **999,999** sided die **10** times"""
+__Please Note__: You are limited to 999999x10, meaning that at maximum you can roll a **999,999** sided die **10** times""".tr(event, event.guild.getPrefix())
         if (arguments.size == 0) event.channel.send(help)
         else {
             val split /* First element is the side count, Second is the amount of rolls */ = arguments[0].split("x")
             if (split.size == 2) {
                 val sides = split[0].toIntOrNull()
                 val rolls = split[1].toIntOrNull()
-                if (sides == null || sides <= 1 || (sides > 999999 && !event.author.isStaff())) event.channel.send("${Emoji.HEAVY_MULTIPLICATION_X} You specified an invalid amount of sides. Please retry")
-                else if (rolls == null || rolls <= 0 || (rolls > 10 && !event.author.isStaff())) event.channel.send("${Emoji.HEAVY_MULTIPLICATION_X} You specified an illegal amount of rolls. Please retry")
+                if (sides == null || sides <= 1 || (sides > 999999 && !event.author.isStaff())) event.channel.send("${Emoji.HEAVY_MULTIPLICATION_X} " + "You specified an invalid amount of sides. Please retry".tr(event))
+                else if (rolls == null || rolls <= 0 || (rolls > 10 && !event.author.isStaff())) event.channel.send("${Emoji.HEAVY_MULTIPLICATION_X} " + "You specified an illegal amount of rolls. Please retry".tr(event))
                 else {
-                    val embed = event.member.embed("Roll Results")
+                    val embed = event.member.embed("Roll Results".tr(event))
                     (1..rolls)
-                            .forEach { embed.appendDescription("Roll **$it**: ${random.nextInt(sides) + 1}\n") }
-                    event.channel.send(embed.appendDescription("\nResults gathered from a __$rolls sided die__"))
+                            .forEach { embed.appendDescription("Roll **{0}**: {1}".tr(event, it, random.nextInt(sides) + 1) + "\n") }
+                    event.channel.send(embed.appendDescription("\n" + "Results gathered from a __{0} sided die__".tr(event, sides)))
                 }
-            } else event.channel.send("Invalid arguments. Please type ${event.guild.getPrefix()}roll to see how to use this command")
+            } else event.channel.send("Invalid arguments. Please type {0}roll to see how to use this command".tr(event, event.guild.getPrefix()))
         }
     }
 }
 
 class UrbanDictionary : Command(Category.FUN, "urban", "get search results for your favorite words from urban dictionary", "ud") {
     override fun execute(arguments: MutableList<String>, event: MessageReceivedEvent) {
-        if (arguments.size == 0) event.channel.send("${Emoji.HEAVY_MULTIPLICATION_X} Bro, you gotta include a search term")
+        if (arguments.size == 0) event.channel.send("${Emoji.HEAVY_MULTIPLICATION_X} " + "You gotta include a search term".tr(event))
         else {
             val term = arguments.concat()
             val search = getGson().fromJson(Jsoup.connect("http://api.urbandictionary.com/v0/define?term=${URLEncoder.encode(term)}").ignoreContentType(true).get()
                     .body().text(), UDSearch::class.java)
-            if (search.list.isEmpty()) event.channel.send("There were no results for this term")
+            if (search.list.isEmpty()) event.channel.send("There were no results for this term :(".tr(event))
             else {
                 val result = search.list[0]
-                event.channel.send(event.member.embed("Urban Dictionary Result: $term")
+                event.channel.send(event.member.embed("Urban Dictionary Definition for {0}".tr(event, term))
                         .setThumbnail("https://i.gyazo.com/6a40e32928743e68e9006396ee7c2a14.jpg")
                         .setColor(Color.decode("#00B7BE"))
-                        .addField("Definition", result.definition.shortenIf(1024), true)
-                        .addField("Example", result.example.shortenIf(1024), true)
-                        .addField("Thumbs Up", result.thumbs_up.toString(), true)
-                        .addField("Thumbs Down", result.thumbs_down.toString(), true)
-                        .addField("Author", result.author, true)
-                        .addField("Permalink", result.permalink, true)
+                        .addField("Definition".tr(event), result.definition.shortenIf(1024), true)
+                        .addField("Example".tr(event), result.example.shortenIf(1024), true)
+                        .addField("Thumbs Up".tr(event), result.thumbs_up.toString(), true)
+                        .addField("Thumbs Down".tr(event), result.thumbs_down.toString(), true)
+                        .addField("Author".tr(event), result.author, true)
+                        .addField("Permalink".tr(event), result.permalink, true)
                 )
             }
         }
@@ -81,7 +81,7 @@ class UnixFortune : Command(Category.FUN, "unixfortune", "in the mood for a unix
 
 class EightBall : Command(Category.FUN, "8ball", "ask the magical 8 ball your future... or something, idfk") {
     override fun execute(arguments: MutableList<String>, event: MessageReceivedEvent) {
-        if (arguments.size == 0) event.channel.send("${Emoji.HEAVY_MULTIPLICATION_X} How dare you try to ask the 8-ball an empty question??!!")
+        if (arguments.size == 0) event.channel.send("${Emoji.HEAVY_MULTIPLICATION_X} " + "How dare you try to ask the 8-ball an empty question??!!".tr(event))
         else {
             try {
                 event.channel.send(getGson().fromJson(Jsoup.connect("https://8ball.delegator.com/magic/JSON/${URLEncoder.encode(arguments.concat())}")
@@ -109,19 +109,19 @@ class Translate : Command(Category.FUN, "translate", "translate text to the prov
         if (arguments.size < 2) {
             val prefix = event.guild.getPrefix()
             event.channel.send("""Using the translation command is simple. The format for requesting one is as follows:
-**${prefix}translate language_code_here your text goes here**
+**{0}translate language_code_here your text goes here**
 
-As follows are the language codes of some languages, but if you don't see the code for the language you want, go to <https://ardentbot.com/translation/languages> to view a full list.
+As follows are the language codes of some languages, but if you don't see the code for the language you want, go to {1} to view a full list.
 **English**: en, **French**: fr, **Spanish**: es, **Russian**: ru
 
-**Example**: *${prefix}translate en Bonjour tout le monde!* will return *Hello everyone!*""")
+**Example**: *{0}translate en Bonjour tout le monde!* will return *Hello everyone!*""".tr(event, prefix, "<https://ardentbot.com/translation/languages>"))
         } else {
             try {
                 val code = arguments[0]
                 arguments.removeAt(0)
                 event.channel.send(api.translationApi().translate(arguments.concat(), Language.of(code)).text()!!)
             } catch (e: Exception) {
-                event.channel.send("You need to include a valid language code! Please visit https://ardentbot.com/translation/languages for a guide")
+                event.channel.send("You need to include a valid language code! Please visit {0} for a guide".tr(event, "<https://ardentbot.com/translation/languages>"))
             }
         }
     }
@@ -135,55 +135,55 @@ class IsStreaming : Command(Category.FUN, "streaming", "check whether someone is
     }
 
     override fun execute(arguments: MutableList<String>, event: MessageReceivedEvent) {
-        if (arguments.size == 0) event.channel.send("Please include the name of a channel - **Example**: `${event.guild.getPrefix()}streaming ardentdiscord`")
+        if (arguments.size == 0) event.channel.send("Please include the name of a channel - **Example**: `{0}streaming ardentdiscord`".tr(event, event.guild.getPrefix()))
         else {
             val ch = arguments.concat()
             twitch.channels().get(ch, object : ChannelResponseHandler {
                 override fun onSuccess(twitchChannel: Channel) {
                     twitch.streams().get(twitchChannel.name, object : StreamResponseHandler {
                         override fun onSuccess(stream: Stream?) {
-                            val embed = event.member.embed("${twitchChannel.name} - on Twitch")
-                                    .addField("Display Name", twitchChannel.displayName, true)
-                                    .addField("Twitch Link", "[Click Here](${twitchChannel.url})", true)
+                            val embed = event.member.embed("{0} on Twitch".tr(event, twitchChannel.name))
+                                    .addField("Display Name".tr(event), twitchChannel.displayName, true)
+                                    .addField("Twitch Link".tr(event), "[Click Here]({0})".tr(event, twitchChannel.url), true)
                             if (stream != null && stream.isOnline) {
                                 embed.setColor(Color.GREEN)
-                                        .addField("Currently Streaming", "true", true)
-                                        .addField("Streaming Game", stream.game, true)
-                                        .addField("Viewers", stream.viewers.format(), true)
-                                        .addField("Average FPS", stream.averageFps.toString(), true)
+                                        .addField("Currently Streaming".tr(event), "yes".tr(event), true)
+                                        .addField("Streaming Game".tr(event), stream.game, true)
+                                        .addField("Viewers".tr(event), stream.viewers.format(), true)
+                                        .addField("Average FPS".tr(event), stream.averageFps.toString(), true)
                                 embed.setImage(stream.preview.medium)
                             } else {
                                 embed.setColor(Color.RED)
-                                        .addField("Currently Streaming", "false", true)
+                                        .addField("Currently Streaming".tr(event), "no".tr(event), true)
                                 if (twitchChannel.videoBanner == null) embed.setThumbnail(twitchChannel.logo)
                                 else embed.setThumbnail(twitchChannel.videoBanner)
                             }
-                            embed.addField("Views", twitchChannel.views.format(), true)
-                                    .addField("Followers", twitchChannel.followers.format(), true)
-                                    .addField("Creation Date", twitchChannel.createdAt.toGMTString(), true)
-                                    .addField("Is Partnered?", twitchChannel.isPartner.toString(), true)
-                                    .addField("Mature Content?", twitchChannel.isMature.toString(), true)
-                                    .addField("Language", twitchChannel.language, true)
+                            embed.addField("Views".tr(event), twitchChannel.views.format(), true)
+                                    .addField("Followers".tr(event), twitchChannel.followers.format(), true)
+                                    .addField("Creation Date".tr(event), twitchChannel.createdAt.toGMTString(), true)
+                                    .addField("Is Partnered?".tr(event), twitchChannel.isPartner.toString(), true)
+                                    .addField("Mature Content?".tr(event), twitchChannel.isMature.toString(), true)
+                                    .addField("Language".tr(event), twitchChannel.language, true)
 
                             event.channel.send(embed)
                         }
 
                         override fun onFailure(failure: Throwable) {
-                            event.channel.send("Unable to retrieve channel data. **Reason**: ${failure.localizedMessage}")
+                            event.channel.send("Unable to retrieve channel data. **Reason**: {0}".tr(event, failure.localizedMessage))
                         }
 
                         override fun onFailure(p0: Int, p1: String?, p2: String?) {
-                            onFailure(Exception("Unknown API Error"))
+                            onFailure(Exception("Unknown API Error".tr(event)))
                         }
                     })
                 }
 
                 override fun onFailure(failure: Throwable) {
-                    event.channel.send("Unable to retrieve channel data. **Reason**: ${failure.localizedMessage}")
+                    event.channel.send("Unable to retrieve channel data. **Reason**: {0}".tr(event, failure.localizedMessage))
                 }
 
                 override fun onFailure(p0: Int, p1: String?, p2: String?) {
-                    onFailure(Exception("Unknown API Error"))
+                    onFailure(Exception("Unknown API Error".tr(event)))
                 }
             })
         }
